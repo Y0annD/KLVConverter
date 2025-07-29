87151969d54f71e6cdb5c086531b9d629edddd53
Yoann Diquélou <yoann.diquelou@gmail.com>
2025-07-29 21:51:09 +0200

    Create Unit test project

---
+++ KLVConverter.Tests/KLV/ST0601/Converter/DefaultDatatypeConverterTests.cs
@@ -0,0 +1,31 @@
+﻿using KLVConverter.KLV.ST0601.Converter;
+
+namespace KLVConverter.Tests.KLV.ST0601.Converter;
+
+public class DefaultDatatypeConverterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestEmptyByteArray()
+    {
+        
+        Assert.That(new DefaultDatatypeConverter().Accept([]), Is.EqualTo("[]"));
+    }
+
+    [Test]
+    public void TestOneItemByteArray()
+    {
+        Assert.That(new DefaultDatatypeConverter().Accept([128]), Is.EqualTo("[128]"));
+    }
+
+
+    [Test]
+    public void TestMultipleItemByteArray()
+    {
+        Assert.That(new DefaultDatatypeConverter().Accept([128, 1, 0, 3]), Is.EqualTo("[128,1,0,3]"));
+    }
+}
